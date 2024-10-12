b4d9bf255a4c0af9c1e0ae9f3a091b1adc55ffd6
张塑 <zhangsu@zhangsudeMBP.lan>
2024-10-11 21:35:24 -0400

    Edited

--- test_main.py
+++ test_main.py
@@ -2,6 +2,10 @@
 Test goes here
 """
 
+import os
+from databricks import sql
+import pandas as pd
+from dotenv import load_dotenv
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
@@ -13,8 +17,29 @@ def test_extract():
 
 
 def test_load():
-    test = load()
-    assert test == "Load Success", "Failed to load the database"
+    load_dotenv()
+    server_h = os.getenv("SERVER_HOST")
+    access_token = os.getenv("ACCESS_TOKEN")
+    http_path = os.getenv("HTTP_PATH")
+    with sql.connect(
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as connection:
+        c = connection.cursor()
+        c.execute("SHOW TABLES FROM default LIKE 'Drinks*'")
+        result1 = c.fetchall()
+        c.execute("SELECT * FROM Drinks")
+        result2 = c.fetchall()
+        c.execute("SHOW TABLES FROM default LIKE 'Countries*'")
+        result3 = c.fetchall()
+        c.execute("SELECT * FROM Countries")
+        result4 = c.fetchall()
+        c.close()
+    assert result1 is not None
+    assert result2 is not None
+    assert result3 is not None
+    assert result4 is not None
 
 
 def test_query():
